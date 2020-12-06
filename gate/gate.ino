f5cdf4bc00e9c442aac99685566a09d5b6af4624
patterncard <patcad@o2.pl>
2020-12-06 15:34:03 +0100

    all LEDs on

--- gate/gate.ino
+++ gate/gate.ino
@@ -1,60 +1,62 @@
-const int ledPin13 = 13;
-const int ledPin12 = 12;
-const int ledPin11 = 11;
-const int ledPin10 = 10;
-const int ledPin9 = 9;
-const int buttonInterruptPin = 3;
-const int buttonOpenClosePin = 2;
+int pinLED13 = 13;
+int pinLED12 = 12;
+int pinLED11 = 11;
+int pinLED10 = 10;
+int pinLED9 = 9;
+int pinSwitchOpenClose = 2;
+ 
+// variables to hold the new and old switch states
+boolean oldSwitchOpenCloseState = LOW;
+boolean newSwitchOpenCloseState = LOW;
+ 
+boolean stateLED13 = LOW;
+boolean stateLED12 = LOW;
+boolean stateLED11 = LOW;
+boolean stateLED10 = LOW;
+boolean stateLED9 = LOW;
 
-int animationSpeed = 0;
-int buttonOpenCloseState = 0;
-int buttonInterruptState = 0;
-
-
-void setup() {
-  pinMode(ledPin13, OUTPUT);
-  pinMode(ledPin12, OUTPUT);
-  pinMode(ledPin11, OUTPUT);
-  pinMode(ledPin10, OUTPUT);
-  pinMode(ledPin9, OUTPUT);
-
-  pinMode(buttonInterruptPin, INPUT); // interrupting 
-  pinMode(buttonOpenClosePin, INPUT); // opening and closing the gate
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    Serial.println(" ");
+ 
+    pinMode(pinLED13, OUTPUT);  
+    pinMode(pinLED12, OUTPUT);  
+    pinMode(pinLED11, OUTPUT);
+    pinMode(pinLED10, OUTPUT);
+    pinMode(pinLED9, OUTPUT);
+    
+    digitalWrite(pinLED13,LOW); 
+    digitalWrite(pinLED12,LOW); 
+    digitalWrite(pinLED11,LOW); 
+    digitalWrite(pinLED10,LOW); 
+    digitalWrite(pinLED9,LOW); 
+ 
+    pinMode(pinSwitchOpenClose, INPUT); 
 }
-
-void loop() {
-  animationSpeed = 400;
-  
-  // read the state of the pushbutton value:
-  buttonOpenCloseState = digitalRead(2);
-  buttonInterruptState = digitalRead(3);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonOpenCloseState == HIGH) {
-    // turn LED on:
-    digitalWrite(13, HIGH);
-    delay(animationSpeed);
-    digitalWrite(12, HIGH);
-    delay(animationSpeed);
-    digitalWrite(11, HIGH);
-    delay(animationSpeed);
-    digitalWrite(10, HIGH);
-    delay(animationSpeed);
-    digitalWrite(9, HIGH);
-    delay(animationSpeed);
-
-  } else {
-    // turn LED off:
-    digitalWrite(13, LOW);
-    delay(animationSpeed);
-    digitalWrite(12, LOW);
-    delay(animationSpeed);
-    digitalWrite(11, LOW);
-    delay(animationSpeed);
-    digitalWrite(10, LOW);
-    delay(animationSpeed);
-    digitalWrite(9, LOW);
-    delay(animationSpeed);
-  }
-  delay(10);
+ 
+void loop()
+{
+    newSwitchOpenCloseState = digitalRead(pinSwitchOpenClose);
+ 
+    if ( newSwitchOpenCloseState != oldSwitchOpenCloseState ) 
+    {
+       // has the button switch been closed?
+       if ( newSwitchOpenCloseState == HIGH )
+       {
+           if ( stateLED13 == LOW ) { digitalWrite(pinLED13, HIGH);  stateLED13 = HIGH; }
+           else                    { digitalWrite(pinLED13, LOW);   stateLED13 = LOW;  }
+           if ( stateLED12 == LOW ) { digitalWrite(pinLED12, HIGH);  stateLED12 = HIGH; }
+           else                    { digitalWrite(pinLED12, LOW);   stateLED12 = LOW;  }
+           if ( stateLED11 == LOW ) { digitalWrite(pinLED11, HIGH);  stateLED11 = HIGH; }
+           else                    { digitalWrite(pinLED11, LOW);   stateLED11 = LOW;  }
+           if ( stateLED10 == LOW ) { digitalWrite(pinLED10, HIGH);  stateLED10 = HIGH; }
+           else                    { digitalWrite(pinLED10, LOW);   stateLED10 = LOW;  }
+           if ( stateLED9 == LOW ) { digitalWrite(pinLED9, HIGH);  stateLED9 = HIGH; }
+           else                    { digitalWrite(pinLED9, LOW);   stateLED9 = LOW;  }
+       }
+       oldSwitchOpenCloseState = newSwitchOpenCloseState;
+    }   
 }
